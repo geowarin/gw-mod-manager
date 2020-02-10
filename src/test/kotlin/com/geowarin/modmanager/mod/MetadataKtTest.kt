cd80e7cb3634c4b205f6a708d51389c0225eedb8
Geoffroy Warin <code@geowarin.com>
2020-02-10 18:52:19 +0100

    Document new lines issue in descriptions

---
+++ src/test/kotlin/com/geowarin/modmanager/mod/MetadataKtTest.kt
@@ -0,0 +1,50 @@
+package com.geowarin.modmanager.mod
+
+import com.gitlab.mvysny.konsumexml.konsumeXml
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class MetadataKtTest {
+
+  @Test
+  fun `parse metadata`() {
+    assertEquals(
+      ModMetaData("", "", "", ""),
+      """<ModMetaData></ModMetaData>""".parseMeta()
+    )
+
+    assertEquals(
+      """line1
+        |line2
+""".trimMargin(),
+      """
+      <ModMetaData>
+        <description>
+        <![CDATA[line1
+      line2
+      ]]>
+        </description>
+      </ModMetaData>
+    """.trimIndent().parseMeta().description
+    )
+
+    // https://gitlab.com/mvysny/konsume-xml/issues/8
+    assertEquals(
+      """line1
+        |line2
+""".trimMargin(),
+      """
+      <ModMetaData>
+        <description>
+        line1
+        line2
+        </description>
+      </ModMetaData>
+    """.trimIndent().parseMeta().description
+    )
+  }
+
+}
+
+private fun String.parseMeta() =
+  this.konsumeXml().child("ModMetaData") { ModMetaData.xml(this) }
